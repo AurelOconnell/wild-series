b8b4c1ab3ef46eec2c19e93c1fcda0a983ae883d
AurelOconnell <aurelienoconnell@gmail.com>
2023-05-22 11:59:16 +0200

    5th quest

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -8,14 +8,21 @@ use Symfony\Component\HttpFoundation\Response;
 
 use Symfony\Component\Routing\Annotation\Route;
 
-
+#[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
-    #[Route('/program/', name: 'program_index')]
+    #[Route('/', name: 'program_index')]
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Series',
          ]);
     }
+
+    #[Route('/{id}', requirements: ['id'=>'\d+'], methods: ['GET'], name: 'show')]
+  public function list(int $id = 1): Response {
+
+    return $this->render('program/show.html.twig', ['id' => $id]);
+
+  }
 }
