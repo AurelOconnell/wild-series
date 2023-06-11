356f380fd12b5194e0004f49b8a07e71d3695a7f
AurelOconnell <aurelienoconnell@gmail.com>
2023-06-11 21:50:30 +0200

    starting quest 19th EntityType

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -3,11 +3,13 @@
 namespace App\Form;
 
 use App\Entity\Program;
+use App\Entity\Actor;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class ProgramType extends AbstractType
 
@@ -21,6 +23,12 @@ class ProgramType extends AbstractType
             ->add('country', TextType::class)
             ->add('year', TextType::class)
             ->add('category', null, ['choice_label' => 'name'])
+            ->add('actors', EntityType::class, [
+                'class' => Actor::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => false,
+            ])
         ;
     }
 
